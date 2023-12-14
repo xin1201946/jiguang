1ca91dc313f8c42eafbb5254f6cf555f4c368dd0
九千七 <2290097907@qq.com>
2023-12-14 10:57:11 +0800

    大屏

--- src/views/DataScreen/dataScreen.vue
+++ src/views/DataScreen/dataScreen.vue
@@ -2,57 +2,66 @@
   <dv-border-box-8 :dur="20" class="DataScreen container">
     <div class="head">
       <div class='top'>
-        <div class="left">
-          <img class="logo" src="../../assets/logo1.png" alt="" />
-          <div>
-            {{data.configName}}
+        <div>
+          <div class="left">
+            <img class="logo" src="../../assets/logo1.png" alt="" />
+            <div>
+              <!-- {{ data.configName }} -->
+              {{ stageName }}-{{ projectName }}-{{ stageGroup }}
+            </div>
           </div>
+          <!-- <div>{{ logoTitle }}</div> -->
         </div>
         <div class='right'>
           <div>
-            <div>比赛名称</div>
-            <div>地点</div>
+            <div class="nameTitle">
+              <div>{{ '1208青少年射击比赛' }}</div>
+              <div style="margin-left: 15px;">{{ "保定市星光酒店" }}</div>
+            </div>
+            <div class="times">2023-12-14 09:06</div>
           </div>
         </div>
       </div>
-      <div class='bottom'>
+      <!-- <div class='bottom'>
         <div>{{ logoTitle }}</div>
-        <div>年月日开始时间</div>
-      </div>
+        <div>时间：2023-12-14</div>
+      </div> -->
     </div>
-<!--    个人资格赛表头-->
+    <!--    个人资格赛表头-->
     <div class='th'>
-      <div v-for='(item, i) in th' :key='i' :style="{width: item.width || `calc((100% - 80px) / ${th.length -1} )`}">
-        {{item.name}}
+      <div v-for='(item, i) in th' :key='i' :style="`width:${item.width};flex:${item.width ? 'none' : '1'}`">
+        {{ item.name }}
       </div>
     </div>
-<!--    前8位-->
+    <!--    前8位-->
     <div class='finalEight'>
       <div v-for="(item, i) in finalEight" :key="i" class="finalEightRow">
-        <div style="width: 80px">{{item.rank}}</div>
-        <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">{{ item.targetSite }}</div>
-        <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">{{ item.playerName }}</div>
-        <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">{{ item.groupName }}</div>
-        <div v-for="k in shootGroups" :key="k" :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">
-          {{item.groupList[k - 1] && item.groupList[k - 1].groupTotal}}
+        <div style="width: 80px">{{ item.rank }}</div>
+        <div style="width: 80px">{{ item.targetSite }}</div>
+        <div style="width: 100px">{{ item.playerName }}</div>
+        <div style="flex: 1;">{{ item.groupName }}</div>
+        <div v-for="k in shootGroups" :key="k" style="width: 80px;">
+          {{ item.groupList[k - 1] && item.groupList[k - 1].groupTotal }}
         </div>
-        <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">{{ item.total }}</div>
-        <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">Q</div>
+        <div style="flex: 1;">{{ item.total }}</div>
+        <div style="flex: 1;">{{ item.notes }}</div>
+        <div style="width: 60px">{{ item.bePromoted }}</div>
       </div>
     </div>
-<!--    后面的滚动-->
+    <!--    后面的滚动-->
     <div class="foot" ref="scorllBox">
       <div class="footContent" ref="scorllArr">
         <div v-for="(item, i) in list" :key="i" class="finalEightRow">
-          <div style="width: 80px">{{item.rank}}</div>
-          <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">{{ item.targetSite }}</div>
-          <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">{{ item.playerName }}</div>
-          <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">{{ item.groupName }}</div>
-          <div v-for="k in shootGroups" :key="k" :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">
-            {{item.groupList[k - 1] && item.groupList[k - 1].groupTotal}}
-          </div>
-          <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}">{{ item.total }}</div>
-          <div :style="{width: `calc((100% - 80px) / ${th.length -1} )`}"></div>
+          <div style="width: 80px">{{ item.rank }}</div>
+        <div style="width: 80px">{{ item.targetSite }}</div>
+        <div style="width: 100px">{{ item.playerName }}</div>
+        <div style="flex: 1;">{{ item.groupName }}</div>
+        <div v-for="k in shootGroups" :key="k" style="width: 80px;">
+          {{ item.groupList[k - 1] && item.groupList[k - 1].groupTotal }}
+        </div>
+        <div style="flex: 1;">{{ item.total }}</div>
+        <div style="flex: 1;">{{ item.notes }}</div>
+        <div style="width: 60px">{{ item.bePromoted }}</div>
         </div>
       </div>
     </div>
@@ -84,14 +93,17 @@ export default {
       // 高度
       hei: 0,
       // 动画定时器
-      animate: undefined
+      animate: undefined,
+      projectName: '',
+      stageGroup: '',
+      stageName: '',
     }
   },
   computed: {
     logoTitle() {
       const data = JSON.parse(window.name)
       const configName = data.configName
-      if (configName.includes("个人资格赛")){
+      if (configName.includes("个人资格赛")) {
         return "个人成绩"
       }
 
@@ -111,28 +123,28 @@ export default {
       const arr2 = document.createElement("div")
       arr2.innerHTML = arr.innerHTML
       const thit = this
-      function scrolls () {
+      function scrolls() {
         box.scrollTop = thit.hei
         if (box.scrollTop > arr.offsetHeight) {
           thit.hei = 0
-        }else {
+        } else {
           // 移动速度
-          thit.hei += 0.5
+          thit.hei += 0.3
         }
         thit.animate = window.requestAnimationFrame(scrolls)
       }
       if (arr.offsetHeight > box.offsetHeight) {
-        if (box.children.length < 2){
+        if (box.children.length < 2) {
           box.appendChild(arr2)
-        }else {
+        } else {
           box.children[1].innerHTML = arr.innerHTML
         }
         if (this.animate === undefined) {
           scrolls()
         }
-      }else {
+      } else {
         box.scrollTop = 0
-        if (box.children[1]){
+        if (box.children[1]) {
           box.removeChild(box.children[1])
         }
         window.cancelAnimationFrame(this.animation)
@@ -144,7 +156,10 @@ export default {
       littleScreen({
         type: this.data.configName
       }).then(res => {
-        const {result} = res
+        const { result } = res
+        this.projectName = result.projectName
+        this.stageGroup = result.stageGroup
+        this.stageName = result.stageName
         // 设置表头
         this.th = [
           {
@@ -153,9 +168,11 @@ export default {
           },
           {
             name: '靶位',
+            width: '80px',
           },
           {
             name: '姓名',
+            width: '100px',
           },
           {
             name: '代表队',
@@ -166,25 +183,30 @@ export default {
           for (let i = 0; i < result.shootGroups; i++) {
             this.th.push({
               name: `${i + 1}0`,
+              width: '80px'
             })
           }
         }
-        this.th.push({ name: '总环数' }, { name: '备注' })
-        if (old === "old"){
+        this.th.push({ name: '总环数' }, { name: '备注' },{ name: '-',width: '60px', })
+        if (old === "old") {
           this.oldList = result.players
         }
         // 设置前8名
-        this.finalEight = result.players.filter(item => item.rank <= 8).map((item,i) => {
+        this.finalEight = result.players.filter(item => item.rank <= 8).map((item, i) => {
           return {
             ...item,
-            total: result.isGood === '是' ? `${item.totalScore}-${item.good}x` : item.totalScore
+            total: result.isGood === '是' ? `${item.totalScore}-${item.good}x` : item.totalScore,
+            notes: '备注.费电还是看会看多少个备注.费电还是看会看多少个备注.费电还是看会看多少个',
+            bePromoted: 'Q',
           }
         })
         // 除了前8名
         this.list = result.players.filter(item => item.rank > 8).map(item => {
           return {
             ...item,
-            total: result.isGood === '是' ? `${item.totalScore}-${item.good}x` : item.totalScore
+            total: result.isGood === '是' ? `${item.totalScore}-${item.good}x` : item.totalScore,
+            notes: '---',
+            bePromoted: '',
           }
         })
         this.$nextTick(() => {
@@ -247,6 +269,7 @@ export default {
 
 <style scoped lang='less'>
 @height: 46px;
+
 .container {
   position: relative;
   height: 100vh;
@@ -256,55 +279,78 @@ export default {
   box-sizing: border-box;
   color: #fff;
 }
-.head{
-  height: 15%;
+
+.head {
+  height: 10%;
   font-size: 24px;
   font-weight: 800;
-  .top{
+
+  .top {
     display: flex;
-    align-items: center;
+    align-items: start;
     justify-content: space-between;
-    .left{
+
+    .left {
       display: flex;
     }
+    .right{
+      .nameTitle{
+        display: flex;
+      }
+      .times{
+        letter-spacing: 6px;
+      }
+    }
   }
-  .bottom{
-    margin-top: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
+
+  // .bottom {
+  //   margin-top: 10px;
+  //   display: flex;
+  //   align-items: center;
+  //   justify-content: space-between;
+  // }
 }
-.th{
+
+.th {
   display: flex;
   justify-content: space-between;
-  font-size: 24px;
-  font-weight: 600;
+  font-size: 26px;
+  font-weight: 800;
   height: 40px;
   line-height: 40px;
-  &>div{
+
+  &>div {
+    flex: 1;
     text-align: center;
   }
 }
-.finalEight{
 
+.finalEight {
+  // display: flex;
+  // justify-content: space-between;
+  padding-bottom: 5px;
+  border-bottom: 1px solid #ccc;
 }
-.finalEightRow{
+
+.finalEightRow {
+  flex: 1;
   display: flex;
   justify-content: space-between;
   height: @height;
   line-height: @height;
-  div{
+
+  div {
     text-align: center;
     font-size: 20px;
-    font-weight: 600;
+    // font-weight: 600;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
   }
 }
-.foot{
-  height: calc(100% - 15% - (@height * 8) - 40px);
+
+.foot {
+  height: calc(100% - 12% - (@height * 8) - 40px);
   overflow: hidden;
 }
 </style>
