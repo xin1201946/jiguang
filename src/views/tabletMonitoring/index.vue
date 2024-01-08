bce89c430f9a8863d6ef77be8c042e1934a8bc3a
九千七 <2290097907@qq.com>
2024-01-08 09:45:33 +0800

    平板监控

---
+++ src/views/tabletMonitoring/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <Card>
+    <template slot="query">
+      <QuerySearch
+        ref="search"
+        @reset="handleSearch"
+        @submit="handleSearch"
+      ></QuerySearch>
+    </template>
+    <div>
+      平板监控
+    </div>
+  </Card>
+</template>
+
+<script>
+import Card from '@comp/card/card.vue'
+import QuerySearch from '@comp/query/QuerySearch.vue'
+export default {
+  name:'tabletMonitoring', //平板监控
+  components:{
+    Card,
+    QuerySearch,
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
