0046a9cf758b3ae4cf68995c42038dcea46313c9
fox <1477773111@qq.com~>
2024-04-23 16:30:28 +0800

    修改打包

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     Crashed when using Webpack `import()` #2463
     https://github.com/vuejs/vue-cli/issues/2463
    */
-  outputDir: "lasergun",
+  outputDir: "exe/lasergun-view",
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: false,
   //qiankuan打包时放开
@@ -26,8 +26,7 @@ module.exports = {
   //   }
   // },
   //打包app时放开该配置
-  // publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  // publicPath: false,
+  publicPath: './',
   configureWebpack: config => {
     //生产环境取消 console.log
     if (process.env.NODE_ENV === 'production') {
